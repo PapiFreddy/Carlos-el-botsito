453d5d9f86337ae86feab3edf723fd6b7aab7a47
SciDroid <scidroid@scidroid.live>
2020-09-29 22:06:11 -0500

    Nuevo Sistema de votaciones

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ cuatro = 0
 
 cinco = 0
 
+participantes = []
+
 bot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
@@ -30,61 +32,55 @@ async def event_ready():
     ws = bot._ws  # this is only needed to send messages within event_ready
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me llego al chat!")
 
-uno = 0
-
-dos = 0
-
-tres = 0
-
-cuatro = 0
-
-cinco = 0
-
 @bot.event
 async def event_message(ctx):
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower() or ctx.author.name.lower() == 'Nightbot'.lower():
+  global participantes
+  if ctx.author.name.lower() == os.environ['BOT_NICK'].lower() or ctx.author.name.lower() == 'Nightbot'.lower():
         return
-    global uno,dos,tres,cuatro,cinco
-    #Contribución de @PythonBoy123
-    await bot.handle_commands(ctx)
+  global uno,dos,tres,cuatro,cinco
+  #Contribución de @PythonBoy123
+  await bot.handle_commands(ctx)
 
     # await ctx.channel.send(ctx.content)
 
     #if 'hola' in ctx.content.lower():
         #await ctx.channel.send(f"Hola, @{ctx.author.name} bienvenido al MEJOR STREAM 2020 espero te la pases bien!!!")
+      #hOLA AA TODOS COMO ESTRAN000
 
-    if 'chakaflus' in ctx.content.lower():
-        await ctx.channel.send("chakaflus")
+  #if 'chakaflus' in ctx.content.lower():
+        #await ctx.channel.send("chakaflus")
 
-    if 'juan-carlos' in ctx.content.lower():
-        await ctx.channel.send("La mejor combinacion 2020")
+  #if 'juan-carlos' in ctx.content.lower():
+        #await ctx.channel.send("La mejor combinacion 2020")
 
-    if 'discord' in ctx.content.lower() and 'sigue a nuestro npc favorito en todas sus redes sociales' not in ctx.content.lower():
-        await ctx.channel.send("Unete al discord mas cool, discord.gg/TWuEDzT ")
+  #if 'discord' in ctx.content.lower() and 'sigue a nuestro npc favorito en todas sus redes sociales' not in ctx.content.lower():
+        #await ctx.channel.send("Unete al discord mas cool, discord.gg/TWuEDzT ")
     
     #if '?' in ctx.content.lower() and 'youtube.com' not in ctx.content.lower():
         #await ctx.channel.send("Contrata a mario")
         
-    if 'ip' in ctx.content.lower():
-        await ctx.channel.send("la IP es 51.161.84.224:25717  y la version es la 1.5.2")
+  #if ' ip' in ctx.content.lower() or 'ip' == ctx.content.lower():
+        #await ctx.channel.send("la IP es 51.161.84.224:25717  y la version es la 1.7.2")
         
-    if 'source' in ctx.content.lower():
-        await ctx.channel.send("quien dijo source?, pues aqui esta el mio https://github.com/scidroid/carlos-el-botsito")
+  if '!source' in ctx.content.lower():
+        await ctx.channel.send("quien dijo source?, pues aqui esta el mio carlos.scidroid.live")
           
-    if ctx.content.lower() == "1":
+  if ctx.content.lower() == "1" and ctx.author.name not in participantes:
         uno += 1
+        participantes.append(ctx.author.name)
 
-    if ctx.content.lower() == "2":
+  if ctx.content.lower() == "2" and ctx.author.name not in participantes:
         dos += 1
-
-    if ctx.content.lower() == "3":
+        participantes.append(ctx.author.name)
+  if ctx.content.lower() == "3" and ctx.author.name not in participantes:
         tres += 1
-
-    if ctx.content.lower() == "4":
+        participantes.append(ctx.author.name)
+  if ctx.content.lower() == "4" and ctx.author.name not in participantes:
         cuatro += 1
-
-    if ctx.content.lower() == "5":
+        participantes.append(ctx.author.name)
+  if ctx.content.lower() == "5" and ctx.author.name not in participantes:
         cinco += 1
+        participantes.append(ctx.author.name)
 #contribucion de christivn.
 
 @bot.command(name="frase")
@@ -94,17 +90,21 @@ async def Frase(ctx):
 
 @bot.command(name="resultados")
 async def resultados(ctx):
-    await ctx.channel.send(f"1 tiene {uno} votos, 2 tiene {dos} votos, 3 tiene {tres} votos, 4 tiene {cuatro} votos, 5 tiene {cinco} votos.")
+    if ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz": 
+      await ctx.channel.send(f"1 tiene {uno} votos, 2 tiene {dos} votos, 3 tiene {tres} votos, 4 tiene {cuatro} votos, 5 tiene {cinco} votos.")
 
 
 
-@bot.command(name="resetvots")
+@bot.command(name="reset")
 async def resetvots(ctx):
+  if ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
+    global uno,dos,tres,cuatro,cinco,participantes
     uno = 0
     dos = 0
     tres = 0
     cuatro = 0
     cinco = 0
+    participantes = []
 
 
 if __name__ == "__main__":
