512bb36f9a140df5cdb71a8dcac3de4df46266b8
SciDroid <scidroid@scidroid.live>
2020-10-24 21:42:30 -0500

    Update bot.py
    el bot esta rehecho, porque no tenia ni idea de como funcionaba lo, comando !vot en trabajo

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import os
 from twitchio.ext import commands
 import random
 import time
+import urllib.request
+import json
 
-
-participantes = []
-
+#declaramos las variables del .ENV
 bot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
@@ -14,191 +14,65 @@ bot = commands.Bot(
     initial_channels=[os.environ['CHANNEL']]
 )
 
-
+#Muestra un mensaje de que el bot esta activo
 @bot.event
-async def event_ready():
+async def llegada():
     'El Bot ha sido llamado'
     print(f"{os.environ['BOT_NICK']} llego al chat!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me llego al chat!")
+    ws = bot._ws
+    await ws.send_privmsg(os.environ['SCI'], f"/me llego al chat!")
 
-
+    
+#declaramos para que el bot ea los mensajes    
 @bot.event
 async def event_message(ctx):
-    global participantes
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower() or ctx.author.name.lower() == 'Nightbot'.lower():
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
         return
-    global uno, dos, tres, cuatro, cinco
-    # Contribución de @PythonBoy123
+
     await bot.handle_commands(ctx)
 
-    if '!source' in ctx.content.lower():
-        await ctx.channel.send("quien dijo source?, pues aqui esta el mio carlos.scidroid.live")
+#declaramos como variable la clave de la Youtube Api
+KEY=os.environ['API']
 
-    if ctx.content.lower() == "1" and ctx.author.name not in participantes:
-        uno += 1
-        participantes.append(ctx.author.name)
+#Comando para saber la cantidad de subs en youtube
+@bot.command(name='subs')
+async def subscriptores(ctx,username):
+    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=" + username + "&key=" + KEY).read()
+    subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+    response = username + " tiene " + "{:,d}".format(int(subs)) + " suscriptores!"
+    await ctx.send(response)
 
-    if ctx.content.lower() == "2" and ctx.author.name not in participantes:
-        dos += 1
-        participantes.append(ctx.author.name)
-    if ctx.content.lower() == "3" and ctx.author.name not in participantes:
-        tres += 1
-        participantes.append(ctx.author.name)
-    if ctx.content.lower() == "4" and ctx.author.name not in participantes:
-        cuatro += 1
-        participantes.append(ctx.author.name)
-    if ctx.content.lower() == "5" and ctx.author.name not in participantes:
-        cinco += 1
-        participantes.append(ctx.author.name)
-# contribucion de christivn.
+#comando para probar que este activo
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.send('test completado!')
 
+#comando para iniciar las votaciones
+@bot.command(name="vot")
+async def vot(ctx,maximo):
+  try:
+    int(maximo)
+  except:
+    await ctx.send("Ingrese un valor numerico")
+  await ctx.send("Comando ejecutado")
+  if maximo == 2:
+    vot_1=0
+    vot_2=2
+    await ctx.send("La votacion se ha establecido en 2")
+  elif maximo == 3:
+    vot_1=0
+    vot_2=0
+    vot_3=0
+    await ctx.send("La votacion se ha establecido en 3")
+  elif maximo == 4:
+    vot_1=0
+    vot_2=0
+    vot_3=0
+    vot_4=0
+    await ctx.send("La votacion se ha establecido en 4")
+  else:
+    await ctx.send("el numero maximo de opciones es 4, ingresa un numero del 2 al 4, EJ: !vot 3")
 
-@bot.command(name="frase")
-async def Frase(ctx):
-    await ctx.channel.send(random.choice('Te quiero mucho', 'La teoria de la gravedad de Einstein', 'Yo no soy senior, ni mid, ni junior yo soy ingeniero', '¿Todo bien en casa?', 'Mis mods son simps', 'Coño *Inserte persona del chat*') + ' -Freddy 2020.')
-
-
-@bot.command(name="votar")
-async def poll(ctx):
-    numero_maximo = ctx.content
-    if numero_maximo == "2" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        uno = 0
-        dos = 0
-        ctx.channel.send("Todos voten poniendo en el chat 1 o 2.")
-    elif numero_maximo == "3" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        uno = 0
-        dos = 0
-        tres = 0
-        ctx.channel.send("Todos voten poniendo en el chat 1, 2 o 3.")
-    elif numero_maximo == "4" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        uno = 0
-        dos = 0
-        tres = 0
-        cuatro = 0
-        ctx.channel.send("Todos voten poniendo en el chat 1, 2, 3 y 4.")
-    elif numero_maximo == "5" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        uno = 0
-        dos = 0
-        tres = 0
-        cuatro = 0
-        cinco = 0
-        ctx.channel.send("Todos voten poniendo en el chat 1, 2, 3, 4 o 5.")
-    else:
-        if ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-            uno = 0
-            dos = 0
-            ctx.channel.send("Todos voten poniendo en el chat 1 o 2.")
-
-
-@bot.command(name="resultados")
-async def resultados(ctx):
-    global numero_maximo
-    if numero_maximo == "2" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        total = uno + dos
-        calculo_porcentaje = total / 100
-        if dos > uno:
-            ganador = 2
-            porcentaje = calculo_porcentaje * dos
-            empate = "no"
-        elif uno > dos:
-            ganador = 1
-            porcentaje = calculo_porcentaje * uno
-            empate = "no"
-        elif uno == dos:
-            empate = "si"
-
-        if empate == "no":
-            ctx.channel.send(
-                f"Gano la opcion {ganador} con el {porcentaje}% de los votos con un total de {total} votos.")
-        elif empate == "si":
-            ctx.channel.send(f"Hay un empate, con un total de {total} votos")
-
-    elif numero_maximo == "3" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        total = uno + dos + tres
-        calculo_porcentaje = total / 100
-        if dos > uno and dos > tres:
-            ganador = 2
-            porcentaje = calculo_porcentaje * dos
-            empate = "no"
-        elif uno > dos and uno > tres:
-            ganador = 1
-            porcentaje = calculo_porcentaje * uno
-            empate = "no"
-        elif tres > dos and tres > uno:
-            ganador = 3
-            porcentaje = calculo_porcentaje * tres
-            empate = "no"
-        elif uno == dos and uno == tres:
-            empate = "si"
-
-        if empate == "no":
-            ctx.channel.send(
-                f"Gano la opcion {ganador} con el {porcentaje}% de los votos con un total de {total} votos.")
-        elif empate == "si":
-            ctx.channel.send(f"Hay un empate, con un total de {total} votos")
-
-    elif numero_maximo == "4" and ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        total = uno + dos + tres + cuatro
-        calculo_porcentaje = total / 100
-        if dos > uno and dos > tres and dos > cuatro:
-            ganador = 2
-            porcentaje = calculo_porcentaje * dos
-            empate = "no"
-        elif uno > dos and uno > tres and uno > cuatro:
-            ganador = 1
-            porcentaje = calculo_porcentaje * uno
-            empate = "no"
-        elif tres > dos and tres > uno and tres > cuatro:
-            ganador = 3
-            porcentaje = calculo_porcentaje * tres
-            empate = "no"
-        elif cuatro > dos and cuatro > uno and cuatro > tres:
-            ganador = 3
-            porcentaje = calculo_porcentaje * tres
-            empate = "no"
-        elif uno == dos and uno == tres and tres == cuatro:
-            empate = "si"
-
-        if empate == "no":
-            ctx.channel.send(
-                f"Gano la opcion {ganador} con el {porcentaje}% de los votos con un total de {total} votos.")
-        elif empate == "si":
-            ctx.channel.send(f"Hay un empate, con un total de {total} votos")
-    else:
-        if ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-            total = uno + dos
-            calculo_porcentaje = total / 100
-            if dos > uno:
-                ganador = 2
-                porcentaje = calculo_porcentaje * dos
-                empate = "no"
-            elif uno > dos:
-                ganador = 1
-                porcentaje = calculo_porcentaje * uno
-                empate = "no"
-            elif uno == dos and uno == tres and tes == cuatro:
-                empate = "si"
-
-            if empate == "no":
-                ctx.channel.send(
-                    f"Gano la opcion {ganador} con el {porcentaje}% de los votos con un total de {total} votos.")
-            elif empate == "si":
-                ctx.channel.send(
-                    f"Hay un empate, con un total de {total} votos")
-
-
-@bot.command(name="reset")
-async def resetvots(ctx):
-    if ctx.author.name.lower() is "freddyfalso" or "scidroid" or "dinosaurio_rar" or "ezequielift" or "pythonlist" or "darkatzzzz" or "emiilrodriguezz":
-        global uno, dos, tres, cuatro, cinco, participantes
-        del uno
-        del dos
-        del tres
-        del cuatro
-        del cinco
-        participantes = []
-
-
+#instruccion de ejecutar el bot  
 if __name__ == "__main__":
-    bot.run()
+  bot.run()
